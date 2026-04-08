9c8ffd9c340c842f3fd85ff8cbb9c80b039f7d67
FLiT3R <pedromauriciocouto@gmail.com>
2026-04-08 10:25:48 +0100

    Scroll Suave para os links de navegação

--- viisual.js
+++ viisual.js
@@ -1,3 +1,7 @@
 // Aguarda que o DOM esteja totalmente carregado
 document.addEventListener('DOMContentLoaded', () => {
 
+    //  Scroll Suave para os links de navegação
+    const links = document.querySelectorAll('nav a');
+    
+    
