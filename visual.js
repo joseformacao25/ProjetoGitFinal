53e64e176059c69d8972e8eebead3836062cbdce
FLiT3R <pedromauriciocouto@gmail.com>
2026-04-08 11:35:22 +0100

    Scroll Suave para os links de navegação

--- visual.js
+++ visual.js
@@ -1,2 +1,22 @@
 // Aguarda que o DOM esteja totalmente carregado
 document.addEventListener('DOMContentLoaded'), () => {
+
+    // Scroll Suave para os links de navegação
+    const links = document.querySelectorAll('nav a');
+    
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+    });
+
+    
